6e66d1a5e475bd106f5add4237353c2a9de1b350
yaghnak20 <151496272+yaghnak20@users.noreply.github.com>
2023-11-20 11:41:56 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,29 @@
+from flask import Flask, render_template, request
 
-from flask import Flask
 app = Flask(__name__)
-@app.route('/')
-def hello():
-"""Return a friendly HTTP greeting."""
-return 'CS8711!'
+
+def celsius_to_fahrenheit(celsius):
+    return celsius * 9/5 + 32
+
+def fahrenheit_to_celsius(fahrenheit):
+    return (fahrenheit - 32) * 5/9
+
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    result = None
+
+    if request.method == 'POST':
+        temperature = float(request.form['temperature'])
+        from_unit = request.form['from_unit']
+        to_unit = request.form['to_unit']
+
+        if from_unit == 'celsius' and to_unit == 'fahrenheit':
+            result = celsius_to_fahrenheit(temperature)
+        elif from_unit == 'fahrenheit' and to_unit == 'celsius':
+            result = fahrenheit_to_celsius(temperature)
+
+    return render_template('index.html', result=result)
+
 if __name__ == '__main__':
-app.run(host='127.0.0.1', port=8080, debug=True)
+    app.run(debug=True)
+
