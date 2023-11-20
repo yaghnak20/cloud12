5bd1f146635dd762d729b0f0a3daba0d5381e87a
yaghnak20 <151496272+yaghnak20@users.noreply.github.com>
2023-11-20 10:49:00 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,9 @@
+
+from flask import Flask
+app = Flask(__name__)
+@app.route('/')
+def hello():
+"""Return a friendly HTTP greeting."""
+return 'CS8711!'
+if __name__ == '__main__':
+app.run(host='127.0.0.1', port=8080, debug=True)
